10509e2eda6b2be2fe59ae2a40b372884a2f2595
NaGit <nislamhridoy152@gmail.com>
2025-08-04 01:54:56 +0600

    Response Status Set

--- src/main/java/com/nakib/firstjobapp/Job/JobController.java
+++ src/main/java/com/nakib/firstjobapp/Job/JobController.java
@@ -1,5 +1,8 @@
 package com.nakib.firstjobapp.Job;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -14,23 +17,23 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll() {
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok( jobService.findAll());
     }
 
     @PostMapping("/jobs")
-    public String createjob(@RequestBody Job job) {
+    public ResponseEntity<String> createjob(@RequestBody Job job) {
         jobService.createJob(job);
-        return "Job Added Successfully";
+        return new ResponseEntity<>( "Job Added Successfully",HttpStatus.CREATED);
     }
 
     @GetMapping("/jobs/{id}")
-    public Job getJobById(@PathVariable  Long id) {
+    public ResponseEntity<Job>  getJobById(@PathVariable  Long id) {
         Job job = jobService.getJobById(id);
         if (job != null)
-            return job;
+            return new ResponseEntity<>(job, HttpStatus.OK);
 
-        return new Job(1L,"Test","Test","2000","5000","Dhaka");
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
 
