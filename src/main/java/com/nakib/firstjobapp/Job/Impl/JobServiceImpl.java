cd8365046f210bcb3f372e6369bb938c6f6a37ac
NaGit <nislamhridoy152@gmail.com>
2025-08-05 23:35:44 +0600

    Delete Operation

--- src/main/java/com/nakib/firstjobapp/Job/Impl/JobServiceImpl.java
+++ src/main/java/com/nakib/firstjobapp/Job/Impl/JobServiceImpl.java
@@ -5,6 +5,7 @@ import com.nakib.firstjobapp.Job.JobService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 @Service
 public class JobServiceImpl implements JobService {
@@ -30,4 +31,19 @@ public class JobServiceImpl implements JobService {
     }
     return null;
   }
+
+  @Override
+  public boolean deleteJobById(Long id) {
+    Iterator<Job> iterator = jobs.iterator();
+    while(iterator.hasNext()){
+      Job job = iterator.next();
+      if(job.getId().equals(id)){
+        iterator.remove();
+        return  true;
+      }
+    }
+    return false;
+  }
+
+
 }
