27dc464cfb57e6698b565c1d083bf62f09852541
NaGit <nislamhridoy152@gmail.com>
2025-08-06 02:30:27 +0600

    RequestMapping

--- src/main/java/com/nakib/firstjobapp/Job/JobController.java
+++ src/main/java/com/nakib/firstjobapp/Job/JobController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/jobs")
 public class JobController {
     private JobService jobService;
 
@@ -16,18 +17,18 @@ public class JobController {
         this.jobService = jobService;
     }
 
-    @GetMapping("/jobs")
+    @GetMapping
     public ResponseEntity<List<Job>> findAll() {
         return ResponseEntity.ok( jobService.findAll());
     }
 
-    @PostMapping("/jobs")
+    @PostMapping
     public ResponseEntity<String> createjob(@RequestBody Job job) {
         jobService.createJob(job);
         return new ResponseEntity<>( "Job Added Successfully",HttpStatus.CREATED);
     }
 
-    @GetMapping("/jobs/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Job>  getJobById(@PathVariable  Long id) {
         Job job = jobService.getJobById(id);
         if (job != null)
@@ -35,7 +36,7 @@ public class JobController {
 
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    @DeleteMapping("/jobs/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteJob(@PathVariable Long id){
         boolean deleted = jobService.deleteJobById(id);
         if(deleted)
@@ -43,7 +44,8 @@ public class JobController {
 
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    @PutMapping("/jobs/{id}")
+    @PutMapping("/{id}")
+    //@RequestMapping(value = "/jobs/{id}" , method = RequestMethod.PUT)
         public ResponseEntity<String> updateJob(@PathVariable Long id, @RequestBody Job updatedJob ){
             boolean updated = jobService.updateJob(id,updatedJob);
             if(updated)
