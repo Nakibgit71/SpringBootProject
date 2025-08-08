ffcb1f280f0f1115923639b26a2f3cd6c0f91187
NaGit <nislamhridoy152@gmail.com>
2025-08-09 01:30:43 +0600

    Modified for JPA

--- src/main/java/com/nakib/firstjobapp/Job/Impl/JobServiceImpl.java
+++ src/main/java/com/nakib/firstjobapp/Job/Impl/JobServiceImpl.java
@@ -1,6 +1,7 @@
 package com.nakib.firstjobapp.Job.Impl;
 
 import com.nakib.firstjobapp.Job.Job;
+import com.nakib.firstjobapp.Job.JobRepository;
 import com.nakib.firstjobapp.Job.JobService;
 import org.springframework.stereotype.Service;
 
@@ -9,40 +10,39 @@ import java.util.Iterator;
 import java.util.List;
 @Service
 public class JobServiceImpl implements JobService {
-  private List<Job> jobs = new ArrayList<>();
+ // private List<Job> jobs = new ArrayList<>();
+  JobRepository jobRepository;
   private Long nextId = 1L;
+
+  public JobServiceImpl(JobRepository jobRepository) {
+    this.jobRepository = jobRepository;
+  }
+
   @Override
   public List<Job> findAll() {
-    return jobs;
+
+    return  jobRepository.findAll();
   }
 
   @Override
   public void createJob(Job job) {
     job.setId(nextId++);
-         jobs.add(job);
+       jobRepository.save(job);
   }
 
   @Override
   public Job getJobById(Long id) {
-    for(Job job:jobs){
-      if(job.getId().equals(id)) {
-        return job;
-      }
-    }
-    return null;
+      return  jobRepository.findById(id).orElse(null);
   }
 
   @Override
   public boolean deleteJobById(Long id) {
-    Iterator<Job> iterator = jobs.iterator();
-    while(iterator.hasNext()){
-      Job job = iterator.next();
-      if(job.getId().equals(id)){
-        iterator.remove();
-        return  true;
-      }
+    try {
+      jobRepository.deleteById(id);
+      return true;
+    } catch (Exception e) {
+      return false;
     }
-    return false;
   }
 
 
